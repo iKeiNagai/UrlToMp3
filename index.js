413df0e3b34c45352bba33a0080de1acb41f5b0d
Thomas <thomasvasquez68@gmail.com>
2025-06-01 15:06:58 -0400

    communicate with endpoint

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+const path = require('path');
+
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/convert', (req, res) => {
+    const { url } = req.body;
+    console.log(`Received URL: ${url}`);
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
