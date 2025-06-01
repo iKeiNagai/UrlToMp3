a37a5df9dc9be81e4a3ba0b5685dd51934c46269
Thomas <thomasvasquez68@gmail.com>
2025-06-01 19:58:13 -0400

    extract and display video title

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const ytdl = require('ytdl-core');
-const ffmpeg = require('fluent-ffmpeg');
+const youtubedl = require('youtube-dl-exec');
 const path = require('path');
 
 const app = express();
@@ -8,9 +7,40 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
-app.post('/convert', (req, res) => {
+async function getVideoData(url) {
+    try{
+        const info = await youtubedl(url, {
+            dumpSingleJson: true,
+            noCheckCertificates: true,
+            preferFreeFormats: true,
+            noWarnings: true,
+        });
+        return info;
+    }catch (error) {
+        console.error('Error fetching metadata:', error);
+        throw error;
+    }
+}
+
+
+app.post('/convert', async (req, res) => {
     const { url } = req.body;
     console.log(`Received URL: ${url}`);
+
+    try{
+        const info = await getVideoData(url);
+        console.log(info.title);
+        
+        const outputPath = path.join(__dirname, 'download', `${info.title}.mp3`);
+        console.log(`Output path: ${outputPath}`);
+
+
+        res.json({title: info.title});
+    }catch (e){
+        console.error('Error processing video:', e);
+        res.status(500).json({ error: 'Failed to process video' });
+    }
+
 })
 
 const PORT = process.env.PORT || 3000;
