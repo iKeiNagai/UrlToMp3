e1f40993a0c9b06196c715173789a0c08b4a835e
Thomas <thomasvasquez68@gmail.com>
2025-06-07 17:00:31 -0400

    download eventhandler reactjs

--- client/src/components/UrlToMp3.jsx
+++ client/src/components/UrlToMp3.jsx
@@ -5,11 +5,14 @@ export default function UrlToMp3() {
     const [url, setUrl] = useState('');
     const [title, setTitle] = useState('');
     const [error, setError] = useState('');
+    const [downloadStart, setDownloadStart] = useState('');
+    const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
     const handleConvert = async (e) => {
         e.preventDefault();
         setError('');
         setTitle('');
+        setDownloadStart('');
 
         try {
 
@@ -33,6 +36,30 @@ export default function UrlToMp3() {
         }
     }
 
+    const handleDownload = async () => {
+        try {
+            const response = await fetch('/save-song', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ url, title }),
+            });
+
+            const data = await response.json();
+
+            if (data.error) {
+                setError(data.error);
+            } else {
+                window.location.href = `${backendUrl}${data.downloadurl}`;
+                setDownloadStart('Download started...');
+            }
+
+        }catch (err) {
+            setError('Cannot connect to the server');
+        }
+    }
+
     return (
         <div>
             <h1>Youtube Url To Mp3</h1>
@@ -48,15 +75,16 @@ export default function UrlToMp3() {
                 <button type='submit'>Convert</button>
             </form>
 
-            {error && <p>{error}</p>}
-
             {title && <p>Title: {title}</p>}
-            {title && (
-                <button>
+
+            {error && <p>{error}</p>}
+            {!error && title && !downloadStart && (
+                <button onClick={handleDownload}>
                     Download
                 </button>
-            )} 
+            )}
 
+            {downloadStart && <p>{downloadStart}</p>}
         </div>
     );
 }
